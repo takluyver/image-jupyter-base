9c37df2ee673188d9fd836a0c70fd83ee1957e85
ReizBe <benedikt.reiz@comma-soft.com>
2019-08-14 11:24:46 +0200

    code_folding_button as option; default=False

--- jupyterfg/jupyterfg/convert.py
+++ jupyterfg/jupyterfg/convert.py
@@ -29,7 +29,7 @@ def add_hide_code(body):
     return re.sub("<body>", "<body>" + hide_code_script, body)
 
 
-def to_html(nb, html_file, exporter=HTMLExporter(template_file="full")):
+def to_html(nb, html_file, exporter=HTMLExporter(template_file="full"), code_folding_button=False):
     """A simple function to convert a notebook to HTML.  For consistency in the output
 format it is used by both, the save hook and the execute & convert function.  The
 exporter is passed as an argument for increased performance (constructing it is time
@@ -38,4 +38,7 @@ consuming) but any valid HTML exporter can be used.
     """
     body, resources = exporter.from_notebook_node(nb)
     with open(html_file, mode="w") as f:
-        f.write(add_hide_code(body))
+        if code_folding_button:
+            f.write(add_hide_code(body))
+        else:
+            f.write(body)
