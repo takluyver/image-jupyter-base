f239b216dc061dc7dac6dc267fd2ac8c3633c10b
mvonpapen <26275671+mvonpapen@users.noreply.github.com>
2019-12-18 14:56:08 +0100

    new hide_code function

--- jupyterfg/jupyterfg/convert.py
+++ jupyterfg/jupyterfg/convert.py
@@ -3,25 +3,27 @@ import re
 
 hide_code_script = """
 <script>
-  function code_toggle() {
-    if (code_shown){
+  // Initiate with code visible
+  codeVisible=true;
+  <!-- toggle code function -->
+  var codeToggle = function code_toggle() {
+    if (codeVisible){
       $('div.input').hide('500');
-      $('#toggleButton').val('Show Code')
+      $('div.output_stderr').hide('500');
+      $('div.output_stdout').hide('500');
+      $('div.output_execute_result').hide('500');
+      $('div.output_prompt').hide('500');
     } else {
       $('div.input').show('500');
-      $('#toggleButton').val('Hide Code')
+      $('div.output_stderr').show('500');
+      $('div.output_stdout').show('500');
+      $('div.output_execute_result').show('500');
+      $('div.output_prompt').show('500');
     }
-    code_shown = !code_shown
+    codeVisible = !codeVisible;
+    return codeVisible;
   }
-
-  $( document ).ready(function(){
-    code_shown=false;
-    $('div.input').hide()
-  });
 </script>
-<form action="javascript:code_toggle()">
-<input type="submit" id="toggleButton" value="Show Code">
-</form>
 """
 
 
@@ -29,16 +31,16 @@ def add_hide_code(body):
     return re.sub("<body>", "<body>" + hide_code_script, body)
 
 
-def to_html(nb, html_file, exporter=HTMLExporter(template_file="full"), code_folding_button=False):
-    """A simple function to convert a notebook to HTML.  For consistency in the output
-format it is used by both, the save hook and the execute & convert function.  The
-exporter is passed as an argument for increased performance (constructing it is time
-consuming) but any valid HTML exporter can be used.
-
+def to_html(nb, html_file, exporter=HTMLExporter(template_file="full"), code_folding=True):
+    """
+	A simple function to convert a notebook to HTML.  For consistency in the output
+    format it is used by both, the save hook and the execute & convert function.  The
+    exporter is passed as an argument for increased performance (constructing it is time
+    consuming) but any valid HTML exporter can be used.
     """
     body, resources = exporter.from_notebook_node(nb, resources={'ipywidgets_base_url': "https://unpkg.com/"})
     with open(html_file, mode="w") as f:
-        if code_folding_button:
+        if code_folding:
             f.write(add_hide_code(body))
         else:
             f.write(body)
