e67307176d34415e10187f41030ef6c3faabb9aa
mvonpapen <26275671+mvonpapen@users.noreply.github.com>
2020-05-12 16:07:02 +0200

    new execute and save

--- jupyterfg/jupyterfg/execute.py
+++ jupyterfg/jupyterfg/execute.py
@@ -1,17 +1,17 @@
-from nbconvert.preprocessors import (
-    ExecutePreprocessor,
-    ClearOutputPreprocessor,
-    CellExecutionError,
-)
-from jupyter_contrib_nbextensions.nbconvert_support import EmbedImagesPreprocessor
+# from nbconvert.preprocessors import (
+#     ExecutePreprocessor,
+#     ClearOutputPreprocessor,
+#     CellExecutionError,
+# )
+# from jupyter_contrib_nbextensions.nbconvert_support import EmbedImagesPreprocessor
 import nbformat
+from nbconvert.preprocessors import ExecutePreprocessor
 from pathlib import Path
 from .convert import to_html
 import logging, sys
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.DEBUG, stream=sys.stdout)
-
 
 def execute_and_save(nb_file, cell_timeout=-1):
     nb_file = Path(nb_file)
@@ -20,25 +20,42 @@ def execute_and_save(nb_file, cell_timeout=-1):
     with open(nb_file) as f:
         nb = nbformat.read(f, as_version=4)
 
-    res = {"metadata": {"path": nb_file.parent}}
-
-    logger.info(f"Stripping output from notebook {nb_file}.")
-    clear = ClearOutputPreprocessor(log_level="DEBUG")
-    nb, _ = clear.preprocess(nb, res)
-
-    logger.info(f"Embedding images in markdown cells for notebook {nb_file}.")
-    embed = EmbedImagesPreprocessor(embed_images=True, embed_remote_images=True)
-    nb, _ = embed.preprocess(nb, res)
-
     logger.info(f"Executing and converting notebook {nb_file} to html.")
-    execute = ExecutePreprocessor(timeout=cell_timeout)
+    ep = ExecutePreprocessor(timeout=cell_timeout)
+    ep.preprocess(nb, {"metadata": {"path": nb_file.parent}})
 
-    with execute.setup_preprocessor(nb, {}):
-        for i, c in enumerate(nb.cells):
-            try:
-                logger.debug(f"Executing cell {i}")
-                c, _ = execute.preprocess_cell(c, {}, i)
-                nb.cells[i] = c
-            to_html(nb, html_file)
-            with open(nb_file, "w") as f:
-                nbformat.write(nb, f)
+    with open(nb_file, "w") as f:
+        nbformat.write(nb, f)
+
+    to_html(nb, html_file)
+
+
+# def execute_and_save(nb_file, cell_timeout=-1):
+#     nb_file = Path(nb_file)
+#     html_file = nb_file.with_suffix(".html")
+#
+#     with open(nb_file) as f:
+#         nb = nbformat.read(f, as_version=4)
+#
+#     res = {"metadata": {"path": nb_file.parent}}
+#
+#     logger.info(f"Stripping output from notebook {nb_file}.")
+#     clear = ClearOutputPreprocessor(log_level="DEBUG")
+#     nb, _ = clear.preprocess(nb, res)
+#
+#     logger.info(f"Embedding images in markdown cells for notebook {nb_file}.")
+#     embed = EmbedImagesPreprocessor(embed_images=True, embed_remote_images=True)
+#     nb, _ = embed.preprocess(nb, res)
+#
+#     logger.info(f"Executing and converting notebook {nb_file} to html.")
+#     execute = ExecutePreprocessor(timeout=cell_timeout)
+#
+#     with execute.setup_preprocessor(nb, {}):
+#         for i, c in enumerate(nb.cells):
+#             try:
+#                 logger.debug(f"Executing cell {i}")
+#                 c, _ = execute.preprocess_cell(c, {}, i)
+#                 nb.cells[i] = c
+#             to_html(nb, html_file)
+#             with open(nb_file, "w") as f:
+#                 nbformat.write(nb, f)
