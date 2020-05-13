d1dda81a4bf0c8046fc6f9e65414ade36b787e1f
Benedikt <benedikt.reiz@comma-soft.com>
2020-05-13 13:29:08 +0200

    :bug: Replace HTMLExporter by EmbedHTMLExporter
    For some reason local images are not embedded into ipynb files in FASTGenomics. By using the EmbedHTMLExporter all images (including external ones) will be rendered into the html file.

--- jupyterfg/jupyterfg/convert.py
+++ jupyterfg/jupyterfg/convert.py
@@ -1,6 +1,6 @@
 import re
 
-from nbconvert import HTMLExporter
+from jupyter_contrib_nbextensions.nbconvert_support import EmbedHTMLExporter
 
 hide_code_script = """
 <script>
@@ -26,7 +26,9 @@ def add_hide_code(body):
     return re.sub("<body>", "<body>" + hide_code_script, body)
 
 
-def to_html(nb, html_file, exporter=HTMLExporter(template_file="full"), code_folding=True):
+def to_html(
+    nb, html_file, exporter=EmbedHTMLExporter(template_file="full"), code_folding=True
+):
     """
     A simple function to convert a notebook to HTML.  For consistency in the output
     format it is used by both, the save hook and the execute & convert function.  The
@@ -40,13 +42,20 @@ def to_html(nb, html_file, exporter=HTMLExporter(template_file="full"), code_fol
     html_file : Path
         Path to the html output file
     exporter : nbconvert exporte, optional
-        The exporter to use, by default HTMLExporter(template_file="full")
+        The exporter to use, by default EmbedHTMLExporter(template_file="full")
     code_folding : bool, optional
         Whether to include the code folding cuntion or not, by default True
     """
-    body, resources = exporter.from_notebook_node(nb, resources={"ipywidgets_base_url": "https://unpkg.com/"})
+    res = {
+        "metadata": {
+            "path": html_file.parent
+        },  # required to embed images correctly, otherwise wrong working directory
+        "ipywidgets_base_url": "https://unpkg.com/",  # required to embed widgets correctly
+    }
+
+    body, resources = exporter.from_notebook_node(nb, resources=res)
     with open(html_file, mode="w") as f:
         if code_folding:
             f.write(add_hide_code(body))
-            else:
-                f.write(body)
+        else:
+            f.write(body)
