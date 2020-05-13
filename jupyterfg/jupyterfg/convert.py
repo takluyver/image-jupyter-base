14276e372c9ef8a06e67cf2dd9b2d291914c8e0e
Benedikt <benedikt.reiz@comma-soft.com>
2020-05-13 09:36:36 +0200

    :books: add docstring

--- jupyterfg/jupyterfg/convert.py
+++ jupyterfg/jupyterfg/convert.py
@@ -26,20 +26,27 @@ def add_hide_code(body):
     return re.sub("<body>", "<body>" + hide_code_script, body)
 
 
-def to_html(
-    nb, html_file, exporter=HTMLExporter(template_file="full"), code_folding=True
-):
+def to_html(nb, html_file, exporter=HTMLExporter(template_file="full"), code_folding=True):
     """
-	A simple function to convert a notebook to HTML.  For consistency in the output
+    A simple function to convert a notebook to HTML.  For consistency in the output
     format it is used by both, the save hook and the execute & convert function.  The
     exporter is passed as an argument for increased performance (constructing it is time
     consuming) but any valid HTML exporter can be used.
+
+    Parameters
+    ----------
+    nb : Path
+        Path to the ipynb inut file
+    html_file : Path
+        Path to the html output file
+    exporter : nbconvert exporte, optional
+        The exporter to use, by default HTMLExporter(template_file="full")
+    code_folding : bool, optional
+        Whether to include the code folding cuntion or not, by default True
     """
-    body, resources = exporter.from_notebook_node(
-        nb, resources={"ipywidgets_base_url": "https://unpkg.com/"}
-    )
+    body, resources = exporter.from_notebook_node(nb, resources={"ipywidgets_base_url": "https://unpkg.com/"})
     with open(html_file, mode="w") as f:
         if code_folding:
             f.write(add_hide_code(body))
-        else:
-            f.write(body)
+            else:
+                f.write(body)
