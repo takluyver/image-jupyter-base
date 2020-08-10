28f34f0d7cf7675be36731d3136d341f109aa7f6
Benedikt <benedikt.reiz@comma-soft.com>
2020-08-10 11:01:21 +0200

    :art: replace deprecated "server_extensions"
    c.NotebookApp.server_extensions --> c.NotebookApp.nbserver_extensions

--- config/jupyter_notebook_config.py
+++ config/jupyter_notebook_config.py
@@ -2,4 +2,4 @@ from jupyterfg.save import post_save_hook
 
 c.FileContentsManager.post_save_hook = post_save_hook
 
-c.NotebookApp.server_extensions.append('crash_ext')
+c.NotebookApp.nbserver_extensions.append("crash_ext")
