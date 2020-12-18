6230831304176374a9502709745d03aef8a7ec66
Benedikt <benedikt.reiz@comma-soft.com>
2020-12-18 13:07:36 +0100

    :bug: Fix file changed on disk error
    Predating `.ipynb`and `.html`files 5 seconds into future after saving.

--- CustomChanges.md
+++ CustomChanges.md
@@ -10,6 +10,7 @@ To handle some issues with jupyter lab some changes were made in our image. It s
 ## jupyterfg
 
 - Widgets were not working in the html, because the righ `ipywidgets_base_url` is not set to `https://unpkg.com/` as a default. The nbconvert CLI is doing this. We added it manually in `convert.py`. There is also an [issue](https://github.com/jupyterlab/jupyterlab/issues/7262) on this.
+- There are multiple [issues](https://github.com/jupyterlab/jupyterlab/issues/7743) because Jupyter reports that the "file changed on disk" since the last save. The reason are different server and client times. There is a 500ms buffer build in, but this is not enough. There are [plans](https://github.com/jupyterlab/jupyterlab/issues/8556) to make this configurable. However, patching the [code](https://github.com/jupyterlab/jupyterlab/blob/34a94a4e65d5606dfb82d33b0f172d579bab5e0b/packages/docregistry/src/context.ts#L647) is not an option, as after clearing caches and rebuiling JLab (e.g., when installing extensions) the values are reset as the package is pulled from the sources again. For this reasons the post save hook is modified such, that the modificaiton date for the `.ipynb` and `.html` file get predated by 5 seconds after save.
 
 ### Open issues
 
--- jupyterfg/jupyterfg/save.py
+++ jupyterfg/jupyterfg/save.py
@@ -1,9 +1,28 @@
 import os
+import time
 from pathlib import Path
 
 import nbformat
 
 from .convert import to_html
+
+
+def mod_time(files: dict, t_offset: int = 5):
+    """Change access and modification time of a file by `offset`.
+
+    To deal with the time buffer issue in Jupyter lab
+
+    Parameters
+    ----------
+    files : dict
+        dict containing the files to modifiy
+    t_offset : int, optional
+        Offset in seconds, by default 5
+    """
+
+    now = round(time.time())
+    for file in files:
+        os.utime(file, (now + t_offset, now + t_offset))
 
 
 def post_save_hook(model, os_path, contents_manager):
@@ -13,11 +32,15 @@ def post_save_hook(model, os_path, contents_manager):
     if model["type"] != "notebook":
         return  # only do this for notebooks
 
-    if not os_path.suffix == '.ipynb': # e.g. for markdown files with jupytext installed
-        return # We might add a converter for markdown Rmarkdown in the future
+    if (
+        not os_path.suffix == ".ipynb"
+    ):  # e.g. for markdown files with jupytext installed
+        return  # We might add a converter for markdown Rmarkdown in the future
 
     with open(os_path) as f:
         nb = nbformat.read(f, as_version=4)
 
     html_file = os_path.with_suffix(".html")
     to_html(nb, html_file)
+
+    mod_time([os_path, html_file])
