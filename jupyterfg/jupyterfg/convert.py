4801cb155536d28fbf05af9701569ad7e5963d91
Benedikt <benedikt.reiz@comma-soft.com>
2021-01-11 09:21:36 +0100

    :bug: Fix template to remove "None" in HTML

--- jupyterfg/jupyterfg/convert.py
+++ jupyterfg/jupyterfg/convert.py
@@ -52,7 +52,7 @@ def to_html(nb, html_file, exporter=None, code_folding=True):
     }
 
     if exporter is None:
-        exporter = EmbedHTMLExporter(template_file="full.tpl")
+        exporter = EmbedHTMLExporter(template="full")
 
     body, resources = exporter.from_notebook_node(nb, resources=res)
     with open(html_file, mode="w") as f:
